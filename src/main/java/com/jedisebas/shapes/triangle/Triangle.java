f7e0620c9c3a61d1db5ccb180cecc1cc97faac3a
Sebastian Dolata <sebasdolata04@gmail.com>
2023-03-09 13:08:35 +0100

    Added Triangle class (#1)
    * Added Triangle class
    
    * Added pre-commit
    
    * Added simple unit test

---
+++ src/main/java/com/jedisebas/shapes/triangle/Triangle.java
@@ -0,0 +1,30 @@
+package com.jedisebas.shapes.triangle;
+
+import com.jedisebas.shapes.Figure;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class Triangle extends Figure {
+
+    final double sideA;
+    final double sideB;
+    final double sideC;
+    final double heightA;
+    final double heightB;
+    final double heightC;
+
+
+    @Override
+    public double area() {
+        return sideA * heightA / 2;
+    }
+
+    @Override
+    public double circumference() {
+        return sideA + sideB + sideC;
+    }
+}
