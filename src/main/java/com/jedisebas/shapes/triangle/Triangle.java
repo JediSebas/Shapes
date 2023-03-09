f9d78437e71c2200ddb82fc7276b39a56854e4e9
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-09 21:34:16 +0100

    Fixed triangle test

--- src/main/java/com/jedisebas/shapes/triangle/Triangle.java
+++ src/main/java/com/jedisebas/shapes/triangle/Triangle.java
@@ -1,13 +1,11 @@
 package com.jedisebas.shapes.triangle;
 
 import com.jedisebas.shapes.Figure;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
-@AllArgsConstructor
 @ToString
 @EqualsAndHashCode(callSuper = false)
 public class Triangle extends Figure {
@@ -19,11 +17,24 @@ public class Triangle extends Figure {
     final double heightB;
     final double heightC;
 
+    public Triangle(final double sideA, final double sideB, final double sideC,
+                    final double heightA, final double heightB, final double heightC) {
+        validateNumber(sideA, sideB, sideC, heightA, heightB, heightC);
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+        this.heightA = heightA;
+        this.heightB = heightB;
+        this.heightC = heightC;
+    }
+
     public static Triangle createEquilateralTriangle(final double side, final double height) {
+        validateNumber(side, height);
         return new Triangle(side, side, side, height, height, height);
     }
 
     public static Triangle createIsoscelesTriangle(final double sideAB, final double sideC, final double heightAB, final double heightC) {
+        validateNumber(sideAB, sideC, heightAB, heightC);
         return new Triangle(sideAB, sideAB, sideC, heightAB, heightAB, heightC);
     }
 
