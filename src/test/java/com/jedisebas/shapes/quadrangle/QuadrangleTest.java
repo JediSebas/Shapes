ef187112995a89537cd478cb2191844a6a06da67
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-09 21:04:20 +0100

    Added quadrangle test

--- src/test/java/com/jedisebas/shapes/quadrangle/QuadrangleTest.java
+++ src/test/java/com/jedisebas/shapes/quadrangle/QuadrangleTest.java
@@ -12,4 +12,9 @@ class QuadrangleTest {
         assertEquals("Quadrangle(sideA=0.0, sideB=0.0, sideC=0.0, sideD=0.0)", quadrangle.toString());
     }
 
+    @Test
+    void givenQuadrangle_whenCalculateArea_returnZero() {
+        Quadrangle quadrangle = new Quadrangle(1, 2, 3, 4);
+        assertEquals(0.0, quadrangle.area());
+    }
 }
