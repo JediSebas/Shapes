1ca43b80b638e7f069b69635a8f7aa3f91355ab4
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-09 15:55:45 +0100

    Added circle tests

---
+++ src/test/java/com/jedisebas/shapes/circle/CircleTest.java
@@ -0,0 +1,27 @@
+package com.jedisebas.shapes.circle;
+
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+class CircleTest {
+
+    @Test
+    void givenRadius_whenCalculateArea_thenReturnArea() {
+        Circle circle = new Circle(4);
+        double area = Math.PI * 4 * 4;
+        assertEquals(area, circle.area());
+    }
+
+    @Test
+    void givenZero_whenCalculateCircumference_thenReturnZero() {
+        Circle circle = new Circle(0);
+        assertEquals(0.0, circle.circumference());
+    }
+
+    @Test
+    void givenCircle_whenToString_thenReturnTrue() {
+        Circle circle = new Circle(1);
+        assertEquals("Circle(radius=1.0)", circle.toString());
+    }
+}
