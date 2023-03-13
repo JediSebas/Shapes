a97afe268264cfcb83b086ddda993863044780d5
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-13 17:24:07 +0100

    Added Prim test

--- src/test/java/com/jedisebas/shapes/solids/prism/PrismTest.java
+++ src/test/java/com/jedisebas/shapes/solids/prism/PrismTest.java
@@ -1,7 +1,22 @@
 package com.jedisebas.shapes.solids.prism;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.jedisebas.shapes.WrongFigureException;
+import com.jedisebas.shapes.quadrangle.Parallelogram;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PrismTest {
 
+    @Test
+    void givenPrism_whenCalculateArea_thenReturnResult() throws WrongFigureException {
+        Parallelogram base = Parallelogram.createSquare(1);
+        double height = 1;
+        Prism prism = new Prism(base, 1);
+
+        double area = base.area() * 2 + height * base.getSideA() + height * base.getSideB() +
+                height * base.getSideC() + height * base.getSideD();
+
+        assertEquals(area, prism.area());
+    }
 }
