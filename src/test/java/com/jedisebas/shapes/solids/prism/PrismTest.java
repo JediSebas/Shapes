23e7861c5ee9634769ea50ec5cfaf7ce79076c89
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-13 17:32:52 +0100

    Added Prism test

--- src/test/java/com/jedisebas/shapes/solids/prism/PrismTest.java
+++ src/test/java/com/jedisebas/shapes/solids/prism/PrismTest.java
@@ -1,6 +1,7 @@
 package com.jedisebas.shapes.solids.prism;
 
 import com.jedisebas.shapes.WrongFigureException;
+import com.jedisebas.shapes.circle.Circle;
 import com.jedisebas.shapes.quadrangle.Parallelogram;
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +19,16 @@ class PrismTest {
                 height * base.getSideC() + height * base.getSideD();
 
         assertEquals(area, prism.area());
+    }
+
+    @Test
+    void givenPrism_whenCalculateCircumference_thenReturnResult() {
+        Circle base = new Circle(2);
+        double height = 13;
+        Prism prism = new Prism(base, height);
+
+        double circumference = base.circumference() * 2;
+
+        assertEquals(circumference, prism.circumference());
     }
 }
