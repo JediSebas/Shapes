2734f723e045a9041bbb700cd08b15c1f0fff73e
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-13 18:33:45 +0100

    Added Prism equals and hashcode

--- src/main/java/com/jedisebas/shapes/solids/prism/Prism.java
+++ src/main/java/com/jedisebas/shapes/solids/prism/Prism.java
@@ -3,9 +3,9 @@ package com.jedisebas.shapes.solids.prism;
 import com.jedisebas.shapes.Figure;
 import com.jedisebas.shapes.circle.Circle;
 import com.jedisebas.shapes.solids.Figure3D;
-import lombok.EqualsAndHashCode;
 
-@EqualsAndHashCode(callSuper = false)
+import java.util.Objects;
+
 public class Prism extends Figure3D {
 
     final Figure base;
@@ -34,5 +34,28 @@ public class Prism extends Figure3D {
     @Override
     public double volume() {
         return base.area() * height;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+
+        if (!(object instanceof final Prism prism)) {
+            return false;
+        }
+
+        return Objects.equals(prism.height, this.height) && Objects.equals(prism.base.area(), this.base.area());
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 21;
+
+        hash = (int) (37 * hash + height);
+        hash = 37 * hash + base.hashCode();
+
+        return hash;
     }
 }
