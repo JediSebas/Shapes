0384a4b229f3f4ad9d38a75370d6b4f05de75cea
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-13 17:14:13 +0100

    Added Figure3D test

--- src/test/java/com/jedisebas/shapes/solids/Figure3DTest.java
+++ src/test/java/com/jedisebas/shapes/solids/Figure3DTest.java
@@ -1,7 +1,30 @@
 package com.jedisebas.shapes.solids;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Figure3DTest {
 
+    @Test
+    void givenFigure3D_whenToString_thenReturnTrue() {
+        Figure3D figure3D = new Figure3D() {
+            @Override
+            public double area() {
+                return 0;
+            }
+
+            @Override
+            public double circumference() {
+                return 0;
+            }
+
+            @Override
+            public double volume() {
+                return 0;
+            }
+        };
+
+        assertEquals("Figure3D()", figure3D.toString());
+    }
 }
