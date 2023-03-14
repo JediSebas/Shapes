56f42057e2f8c751d304283245ce2efed73de8ae
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-14 10:30:59 +0100

    Deleted lombok equals and hashcode in Cube

--- src/main/java/com/jedisebas/shapes/solids/prism/Cube.java
+++ src/main/java/com/jedisebas/shapes/solids/prism/Cube.java
@@ -2,9 +2,7 @@ package com.jedisebas.shapes.solids.prism;
 
 import com.jedisebas.shapes.WrongFigureException;
 import com.jedisebas.shapes.quadrangle.Parallelogram;
-import lombok.EqualsAndHashCode;
 
-@EqualsAndHashCode(callSuper = true)
 public class Cube extends Prism {
 
     public Cube(final double side) throws WrongFigureException {
