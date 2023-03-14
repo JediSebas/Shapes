891a703939f1ccb9faa2d50379d01166c0fca235
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-14 16:07:54 +0100

    Added Cone and PyramidFactory

--- src/main/java/com/jedisebas/shapes/solids/pyramid/Pyramid.java
+++ src/main/java/com/jedisebas/shapes/solids/pyramid/Pyramid.java
@@ -1,17 +1,16 @@
 package com.jedisebas.shapes.solids.pyramid;
 
 import com.jedisebas.shapes.Figure;
-import com.jedisebas.shapes.circle.Circle;
 import com.jedisebas.shapes.solids.Figure3D;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = false)
 public class Pyramid extends Figure3D {
 
-    final Figure base;
-    final double height;
+    protected final Figure base;
+    protected final double height;
 
-    public Pyramid(final Figure base, final double height) {
+    protected Pyramid(final Figure base, final double height) {
         validateNumber(height);
         validateBase(base);
         this.base = base;
@@ -20,12 +19,7 @@ public class Pyramid extends Figure3D {
 
     @Override
     public double area() {
-        if (base.getClass() == Circle.class) {
-            return base.area() + Math.sqrt(Math.pow(((Circle) base).getRadius() / 2, 2) + Math.pow(height, 2)) * base.circumference();
-        } else {
-            return base.area() + Math.sqrt(Math.pow((base.circumference() / base.getCorners()) / 2, 2) + Math.pow(height, 2)) * base.circumference();
-        }
-
+        return base.area() + Math.sqrt(Math.pow((base.circumference() / base.getCorners()) / 2, 2) + Math.pow(height, 2)) * base.circumference();
     }
 
     @Override
