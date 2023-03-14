19812b281c95c8a716337e07f9e6c464cec536f2
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-14 10:35:23 +0100

    Added Pyramid class

---
+++ src/main/java/com/jedisebas/shapes/solids/pyramid/Pyramid.java
@@ -0,0 +1,30 @@
+package com.jedisebas.shapes.solids.pyramid;
+
+import com.jedisebas.shapes.Figure;
+import com.jedisebas.shapes.solids.Figure3D;
+
+public class Pyramid extends Figure3D {
+
+    final Figure base;
+    final double height;
+
+    public Pyramid(final Figure base, final double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return 0;
+    }
+
+    @Override
+    public double circumference() {
+        return 0;
+    }
+
+    @Override
+    public double volume() {
+        return 0;
+    }
+}
