0404b89b66daf2f68c1c0ba3219554a748f8c22e
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-14 10:15:00 +0100

    Added Cube class

---
+++ src/main/java/com/jedisebas/shapes/solids/prism/Cube.java
@@ -0,0 +1,13 @@
+package com.jedisebas.shapes.solids.prism;
+
+import com.jedisebas.shapes.WrongFigureException;
+import com.jedisebas.shapes.quadrangle.Parallelogram;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+public class Cube extends Prism {
+
+    public Cube(final double side) throws WrongFigureException {
+        super(Parallelogram.createSquare(side), side);
+    }
+}
