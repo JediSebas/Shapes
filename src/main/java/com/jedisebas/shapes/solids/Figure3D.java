62408c0a1d1ea61a9fb7642b82919a074a406f80
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-14 14:55:16 +0100

    Updated Figure3D class

--- src/main/java/com/jedisebas/shapes/solids/Figure3D.java
+++ src/main/java/com/jedisebas/shapes/solids/Figure3D.java
@@ -5,6 +5,20 @@ import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 @ToString
-@EqualsAndHashCode
+@EqualsAndHashCode(callSuper = false)
 public abstract class Figure3D extends Figure implements IVolume {
+
+    public static void validateBase(final Figure figure) {
+        if (figure == null) {
+            throw new IllegalArgumentException("You cannot use null base");
+        }
+    }
+
+    /**
+     * In this case we understand circumference as the sum of every side
+     */
+    @Override
+    public double circumference() {
+        return 0;
+    }
 }
