5e87e5e97a36f67c5b0f8789d10f2804c73f4979
sebastian.dolata <sebasdolata04@gmail.com>
2023-03-14 14:57:06 +0100

    Updated Circle class

--- src/main/java/com/jedisebas/shapes/circle/Circle.java
+++ src/main/java/com/jedisebas/shapes/circle/Circle.java
@@ -2,16 +2,21 @@ package com.jedisebas.shapes.circle;
 
 
 import com.jedisebas.shapes.Figure;
-import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
-@AllArgsConstructor
 @ToString
+@EqualsAndHashCode(callSuper = false)
 public class Circle extends Figure {
 
     final double radius;
+
+    public Circle(final double radius) {
+        validateNumber(radius);
+        this.radius = radius;
+    }
 
     @Override
     public double area() {
