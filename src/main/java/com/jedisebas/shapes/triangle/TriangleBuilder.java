9897a3b7878bbbce71a0c7b35a292a253ba22bb3
Sebastian Dolata <sebasdolata04@gmail.com>
2023-03-15 08:55:25 +0100

    Added TriangleBuilder and TriangleChainBuilder (#30)
    * Added TriangleBuilder and TriangleChainBuilder
    
    * Added Singleton for TriangleBuilder and TriangleChainBuilder
    
    * Small fix
    
    * Added synchronized
    
    * Deleted singleton

---
+++ src/main/java/com/jedisebas/shapes/triangle/TriangleBuilder.java
@@ -0,0 +1,25 @@
+package com.jedisebas.shapes.triangle;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TriangleBuilder {
+
+    private double sideA;
+    private double sideB;
+    private double sideC;
+    private double heightA;
+    private double heightB;
+    private double heightC;
+
+    public static TriangleBuilder getBuilder() {
+        return new TriangleBuilder();
+    }
+
+    public Triangle build() {
+        return new Triangle(sideA, sideB, sideC, heightA, heightB, heightC);
+    }
+}
---
+++ src/main/java/com/jedisebas/shapes/triangle/TriangleChainBuilder.java
@@ -0,0 +1,54 @@
+package com.jedisebas.shapes.triangle;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TriangleChainBuilder {
+
+    private double sideA;
+    private double sideB;
+    private double sideC;
+    private double heightA;
+    private double heightB;
+    private double heightC;
+
+    public static synchronized TriangleChainBuilder getBuilder() {
+        return new TriangleChainBuilder();
+    }
+
+    public Triangle build() {
+        return new Triangle(sideA, sideB, sideC, heightA, heightB, heightC);
+    }
+
+
+    public TriangleChainBuilder setSideA(double sideA) {
+        this.sideA = sideA;
+        return this;
+    }
+
+    public TriangleChainBuilder setSideB(double sideB) {
+        this.sideB = sideB;
+        return this;
+    }
+
+    public TriangleChainBuilder setSideC(double sideC) {
+        this.sideC = sideC;
+        return this;
+    }
+
+    public TriangleChainBuilder setHeightA(double heightA) {
+        this.heightA = heightA;
+        return this;
+    }
+
+    public TriangleChainBuilder setHeightB(double heightB) {
+        this.heightB = heightB;
+        return this;
+    }
+
+    public TriangleChainBuilder setHeightC(double heightC) {
+        this.heightC = heightC;
+        return this;
+    }
+}
